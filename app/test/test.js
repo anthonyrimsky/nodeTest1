b44b16a2bc05a4810deb44a031323a793dd2b798
Anthony van den Berg <anthony@arkia.nl>
2016-11-21 16:41:09 +0100

    Extra test

--- app/test/test.js
+++ app/test/test.js
@@ -3,7 +3,7 @@ describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
       //assert.equal(-1, [1,2,3].indexOf(4));
-      //we gaan het zien
+      //we gaan het zien 
       assert.equal(0, [1,2,3].indexOf(4));
     });
   });
